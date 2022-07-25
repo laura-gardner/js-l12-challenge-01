7bb367743d7dad6de5f790a9633c0ce496139160
laura-gardner <elgardner@gmail.com>
2022-07-25 11:18:13 +0100

    added functions to select and display random image

--- js/script.js
+++ js/script.js
@@ -3,10 +3,24 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+
 const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images)
 }; 
 
-getImage();
+button.addEventListener("click", getImage);
